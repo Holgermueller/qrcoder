2edb7d724fe3430b83002069e61019917dae788f
holgermueller <9jholger@gmail.com>
2022-09-01 16:42:13 -0500

    form pretty much works

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-let qrcode = new QRCode("qrcode");
+const error = document.getElementById("error");
 
 const makeCode = () => {
-  qrcode.clear();
-
   const text = document.getElementById("text").value;
+  const size = document.getElementById("size").value;
+  resetQRDIV();
 
-  new QRCode(document.getElementById("qrcode"), {
-    text: text,
-    width: "100",
-    height: "100",
-  });
+  if (text === "") {
+    showError();
+  } else {
+    new QRCode(document.getElementById("qrcode"), {
+      text: text,
+      width: size,
+      height: size,
+    });
 
-  resetForm();
+    hideError();
+    resetForm();
+  }
+};
+
+const resetQRDIV = () => {
+  const qrcode = document.getElementById("qrcode");
+
+  qrcode.innerHTML = "";
 };
 
 const resetForm = () => {
   document.getElementById("text").value = "";
+  document.getElementById("size").selectedIndex = 0;
+};
+
+const showError = () => {
+  error.style.display = "block";
+};
+
+const hideError = () => {
+  error.style.display = "none";
 };
